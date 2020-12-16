403cc198dff07e35e3fb7038d641419e40304d73
Benjamin Kramskoi <ben.kramskoi@gmail.com>
2020-12-16 11:22:46 -0500

    last commit

--- UFO-level-1/static/app.js
+++ UFO-level-1/static/app.js
@@ -1,14 +1,8 @@
 // from data.js
 var tableData = data;
 var tbody = d3.select("tbody");
-// var button = d3.select("#filter-btn");
-// var inputField1 = d3.select("#datetime");
-// var inputField2 = d3.select("#city");
-// var resetbtn = d3.select("#reset-btn");
-// var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
 function ufotable(data) {
     tbody.html("");
-    // BONUS: Refactor to use Arrow Functions!
 data.forEach((UFOData) => {
     var row = tbody.append("tr");
     Object.values(UFOData).forEach(( value) => {
