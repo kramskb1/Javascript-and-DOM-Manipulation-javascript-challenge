440e27683507ed14dbce57a3e8c39f40f3eff972
Benjamin Kramskoi <ben.kramskoi@gmail.com>
2020-12-18 14:09:21 -0500

    last commit

---
+++ UFO-level-2/static/app.js
@@ -0,0 +1,21 @@
+// from data.js
+var tableData = data;
+console.log(tableData);
+
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// UFO Sighting values for each column
+tableData.forEach(function(ufoSighting) {
+    console.log(ufoSighting);
+    // Append one table row `tr` for each UFO Sighting object
+    var row = tbody.append("tr");
+
+    // Use `Object.entries` to console.log each UFO Sighting value
+    Object.entries(ufoSighting).forEach(function([key, value]) {
+      console.log(key, value);
+      // Append a cell to the row for each value
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
